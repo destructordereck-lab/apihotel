6b886797b8ed07e27b5fa63a312ab69b18dfbfdf
destructordereck-lab <destructordereck@gmail.com>
2026-04-10 16:33:38 -0500

    Fix CORS preflight en index.php

--- index.php
+++ index.php
@@ -4,6 +4,15 @@ header('Content-type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT,PATCH, DELETE");
+// Manejo de preflight CORS
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+    http_response_code(200);
+    exit;
+}
+
 //fin cabecera
 
 // Kickstart the framework
